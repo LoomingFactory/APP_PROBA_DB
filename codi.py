69f5ce343ff69ad86b57616ce65363b540a22f45
LoomingFactory <109733709+LoomingFactory@users.noreply.github.com>
2022-07-25 15:41:01 +0200

    Create codi.py

---
+++ codi.py
@@ -0,0 +1,38 @@
+import streamlit as st
+from streamlit_option_menu import option_menu
+
+from datetime import datetime
+import pandas as pd
+import pymongo
+
+
+# --------------- CONNECTO MONGO: Uses st.experimental_singleton to only run once ---------------
+@st.experimental_singleton
+def init_connection():
+    return pymongo.MongoClient("mongodb://looming:012345@123.45.67.89/ProbaConn") #"mongodb://xxxxxxxxxx:yyyyy/" on xxx... = ip públic del ordi al q es connecta // yy = port on esta el mongodb
+    #"mongodb://looming:012345@123.45.67.89/ProbaConn"
+    #"mongodb://147.83.48.118:27017/"
+try:
+    client = init_connection()
+    st.info("s'ha connectat correctament a la base de dades")
+except pymongo.errors.ServerSelectionTimeoutError as errorTiempo:
+    st.error("Temps de connexio al MongoDB excedit. Temps: "+errorTiempo)
+except pymongo.errors.ConnectionFailure as errorConexion:
+    st.error("Error al connectarse a MongoDB. Error: "+errorConexion)
+except pymongo.errors.ConfigurationError as errorConfig:
+    st.error("Error de configuracio de MongoDB. Error: "+errorConfig)
+except pymongo.errors.OperationFailure as errorOperacio:
+    st.error("Error d'operacio de MongoDB. Error: "+errorOperacio)
+except pymongo.errors.InvalidURI as errorURI:
+    st.error("Error de URI de MongoDB. Error: "+errorURI)
+except pymongo.errors.PyMongoError as errorPyMongo:
+    st.error("Error de PyMongo. Error: "+errorPyMongo)
+
+
+
+
+db = client.ProbaConn.Eines1
+buscar = db.find()
+items = list(buscar)  # make hashable for st.experimental_memo
+df = pd.DataFrame(items)
+st.dataframe(df.head())
